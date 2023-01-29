7ee1323f7f3ace67d9c8ade13a6cdfb8ea4be66d
khatanboyevelbek <programmerelbek@gmail.com>
2023-01-29 16:37:34 +0500

    BROKERS: Implament Logging Broker

---
+++ ODS.Web/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,12 @@
+﻿namespace ODS.Web.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogInformation(string message);
+        void LogDebug(string message);
+        void LogError(string message);
+        void LogWarning(string message);
+        void LogTrace(string message);
+        void LogCritical(Exception exception);
+    }
+}
---
+++ ODS.Web/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,28 @@
+﻿namespace ODS.Web.Brokers.Loggings
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        private readonly ILogger logger;
+
+        public LoggingBroker(ILogger<LoggingBroker> logger) =>
+             this.logger = logger;
+
+        public void LogCritical(Exception exception) =>
+            this.logger.LogCritical(exception, exception.Message);
+
+        public void LogDebug(string message) =>
+            this.logger.LogDebug(message);
+
+        public void LogError(string message) => 
+            this.logger.LogError(message);
+
+        public void LogInformation(string message) => 
+            this.logger.LogInformation(message);
+
+        public void LogTrace(string message) => 
+            this.logger.LogTrace(message);
+
+        public void LogWarning(string message) => 
+            this.logger.LogWarning(message);
+    }
+}
