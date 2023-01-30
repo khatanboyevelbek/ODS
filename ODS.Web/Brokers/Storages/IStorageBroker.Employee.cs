e44b277f1ae8e3c5c95f6504bd9f231bbca3d4df
khatanboyevelbek <programmerelbek@gmail.com>
2023-01-30 12:58:05 +0500

    BROKERS: Insert Employee

--- ODS.Web/Brokers/Storages/IStorageBroker.Employee.cs
+++ ODS.Web/Brokers/Storages/IStorageBroker.Employee.cs
@@ -1,6 +1,9 @@
-﻿namespace ODS.Web.Brokers.Storages
+﻿using ODS.Web.Models;
+
+namespace ODS.Web.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<Employee> InsertEmployeeAsync(Employee employee);
     }
 }
--- ODS.Web/Brokers/Storages/StorageBroker.Employee.cs
+++ ODS.Web/Brokers/Storages/StorageBroker.Employee.cs
@@ -6,5 +6,13 @@ namespace ODS.Web.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<Employee> Employees { get; set; }
+
+        public async ValueTask<Employee> InsertEmployeeAsync(Employee employee)
+        {
+            var broker = new StorageBroker(this.Configuration);
+            broker.Entry(employee).State = EntityState.Added;
+            await broker.SaveChangesAsync();
+            return employee;
+        }
     }
 }
