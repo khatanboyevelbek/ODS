1ad5049b100762fd377b036d9238c2753c29289e
khatanboyevelbek <programmerelbek@gmail.com>
2023-02-09 19:03:10 +0500

    CONTROLLERS: Update Actions

--- ODS.Web/Controllers/HomeController.cs
+++ ODS.Web/Controllers/HomeController.cs
@@ -1,23 +1,23 @@
-﻿using System.Diagnostics;
+﻿using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ODS.Web.Brokers.Loggings;
-using ODS.Web.Models;
 using ODS.Web.Models.Foundations;
 using ODS.Web.Models.Foundations.Exceptions;
-using ODS.Web.Services.Processings.Employees;
+using ODS.Web.Services.Orchestrations.Employees;
 
 namespace ODS.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILoggingBroker loggingBroker;
-        private readonly IEmployeeProcessingService employeeProcessingService;
+        private readonly IEmployeeOrchestrationServices employeeOrchestrationServices;
 
         public HomeController(ILoggingBroker loggingBroker,
-            IEmployeeProcessingService employeeProcessingService)
+            IEmployeeOrchestrationServices employeeOrchestrationServices)
         {
             this.loggingBroker = loggingBroker;
-            this.employeeProcessingService = employeeProcessingService;
+            this.employeeOrchestrationServices = employeeOrchestrationServices;
         }
 
         public IActionResult Index()
@@ -34,7 +34,7 @@ namespace ODS.Web.Controllers
                 if (ModelState.IsValid)
                 {
                     int rows =
-                        await this.employeeProcessingService.ImportExternalFileToTable(postedFile);
+                        await this.employeeOrchestrationServices.ImportExternalFileToTable(postedFile);
 
                     TempData["success"] = string.Format("{0} rows added successfuly", rows);
 
@@ -49,38 +49,31 @@ namespace ODS.Web.Controllers
                 TempData["error"] = $"{exception.Message}";
                 return View();
             }
-            catch(FormatException exception)
+            catch(InvalidCastException exception)
             {
-                return RedirectToAction("Error", exception);
+                return RedirectToAction("Error");
+            }
+            catch (FormatException exception)
+            {
+                return RedirectToAction("Error");
+            }
+            catch (Exception exception)
+            {
+                return RedirectToAction("Error");
             }
         }
 
         public IActionResult Data(string orderby = null)
         {
-            IQueryable<Employee> ascendingOrderedEmployees = null;
-
-            IQueryable<Employee> employees =
-                this.employeeProcessingService.RetrieveAllEmployees();
-
-            switch(orderby)
-            {
-                case "firstname":
-                    ascendingOrderedEmployees = employees.OrderBy(x => x.FirstName);
-                    break;
-                case "lastname":
-                    ascendingOrderedEmployees = employees.OrderBy(x => x.LastName);
-                    break;
-                default:
-                    ascendingOrderedEmployees = employees;
-                    break;
-            }
+            IQueryable<Employee> ascendingOrderedEmployees =
+                this.employeeOrchestrationServices.RetrieveEmployeeAscendingOrder(orderby);
 
             return View(ascendingOrderedEmployees);
         }
 
-        public IActionResult Error(Exception exception)
+        public IActionResult Error()
         {
-            return View(exception);
+            return View();
         }
     }
 }
