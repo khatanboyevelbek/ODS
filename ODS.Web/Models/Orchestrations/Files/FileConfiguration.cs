a5bc61f71c534eaa05a9b6071ca6ce3613f9e0b0
khatanboyevelbek <programmerelbek@gmail.com>
2023-02-09 21:36:10 +0500

    ORCHESTRATIONS: Create FileConfiguration

---
+++ ODS.Web/Models/Orchestrations/Files/FileConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace ODS.Web.Models.Orchestrations.Files
+{
+    public class FileConfiguration
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+    }
+}
