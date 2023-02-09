b8855122b967371b7bd3ed08f3b991ddae1f16e8
khatanboyevelbek <programmerelbek@gmail.com>
2023-02-09 19:04:30 +0500

    ORCHESTRATIONS: RetrieveEmployeeAscendingOrder()

---
+++ ODS.Web/Services/Orchestrations/Employees/EmployeeOrchestrationServices.cs
@@ -0,0 +1,38 @@
+﻿using ODS.Web.Brokers.Loggings;
+using ODS.Web.Models.Foundations;
+using ODS.Web.Services.Processings.Employees;
+
+namespace ODS.Web.Services.Orchestrations.Employees
+{
+    public class EmployeeOrchestrationServices : IEmployeeOrchestrationServices
+    {
+        private readonly ILoggingBroker loggingBroker;
+        private readonly IEmployeeProcessingService employeeProcessingService;
+
+        public EmployeeOrchestrationServices(
+            IEmployeeProcessingService employeeProcessingService, 
+            ILoggingBroker loggingBroker)
+        {
+            this.employeeProcessingService = employeeProcessingService;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<int> ImportExternalFileToTable(IFormFile postedFile) => 
+            this.employeeProcessingService.ImportExternalFileToTable(postedFile);
+
+        public IQueryable<Employee> RetrieveEmployeeAscendingOrder(string? orderby)
+        {
+            IQueryable<Employee> employees =
+                this.employeeProcessingService.RetrieveAllEmployees();
+
+            IQueryable<Employee> ascendingOrderedEmployees = orderby switch
+            {
+                "firstname" => employees.OrderBy(x => x.FirstName),
+                "lastname" => employees.OrderBy(x => x.LastName),
+                _ => employees
+            };
+
+            return ascendingOrderedEmployees;
+        }
+    }
+}
---
+++ ODS.Web/Services/Orchestrations/Employees/IEmployeeOrchestrationServices.cs
@@ -0,0 +1,10 @@
+﻿using ODS.Web.Models.Foundations;
+
+namespace ODS.Web.Services.Orchestrations.Employees
+{
+    public interface IEmployeeOrchestrationServices
+    {
+        IQueryable<Employee> RetrieveEmployeeAscendingOrder(string? orderby);
+        ValueTask<int> ImportExternalFileToTable(IFormFile postedFile);
+    }
+}
